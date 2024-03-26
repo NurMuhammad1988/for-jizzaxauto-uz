7cb3642757a54a7843d55ef30374ecf375bd49a0
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-03-26 09:43:15 +0500

    day 5

--- utils/index.ts
+++ utils/index.ts
@@ -1,44 +1,4 @@
-// const axios = require('axios');
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
-//   params: {model: 'corolla'},
-//   headers: {
-//     // 'X-RapidAPI-Key': 'b24dd1ad3bmsh959c49b96b3e0dbp1024f2jsn34dde71fcae3',
-//     'X-RapidAPI-Key': 'KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA',
-
-//     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-//   }
-// };
-
-// try {
-// 	const response = await axios.request(options);
-// 	console.log(response.data);
-// } catch (error) {
-// 	console.error(error);
-// }
-
-// //////////////////////////////////////////
-// const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
-// const options = {
-//     method: "GET",
-//     headers: {
-//         // 'X-RapidAPI-Key': 'b24dd1ad3bmsh959c49b96b3e0dbp1024f2jsn34dde71fcae3',
-//         "X-RapidAPI-Key": "KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA",
-//         "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-//     },
-// };
-
-// try {
-//     const response = await fetch(url, options);
-//     const result = await response.text();
-//     console.log(result);
-// } catch (error) {
-//     console.error(error);
-// }
-// //////////////////////////////////////////
-
+import { CarProps } from "@/ts";
 
 
 
@@ -64,24 +24,6 @@ export async function fetchCars() {
 }
 // "X-RapidAPI-Key": "KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA",//bu Adrianni api keyi !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! BU KEYHAM ISHLADI AGAR BIR OYDAN KEYIN O'ZIMNI KEYIM ISHLAMAY QOLSA ADRIANNI KEYINI ISHLATSAMHAM BO'LISHI MUMKUN CHUNKI ADRIANNI KEYIHAM ISHLADI BUGUN SANA 2024 YIL 24 CHI MART !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 export const calculateCarRent = (city_mpg: number, year: number) => {
     // apidan kelayotgan malumotlarga qo'shildi
     const basePricePerDay = 50; // Dollarda kuniga asosiy ijara narxi
@@ -97,3 +39,7 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   
     return rentalRatePerDay.toFixed(0);
   };
+
+  export const genereteCarImageUrl = (car:CarProps,angle?:string) =>{
+
+  }
